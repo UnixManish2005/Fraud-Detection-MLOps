e44444468a4b9bbc2592cfc69c4a8d2faaaef0e5
Manishankar Dey <manishankardey2005@gmail.com>
2026-03-21 23:22:19 +0530

    Dataset download code is added

---
+++ src/download_data.py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+def download_dataset():
+    
+    file_id = "18KG_dlqbsp_n2fe3wQtHEsUdZrmremiH"
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    
+    save_path = "data/creditcard.csv"
+    
+    if not os.path.exists('data'):
+        os.makedirs('data')
+        
+    if not os.path.exists(save_path):
+        print("Downloading dataset from Google Drive...")
+        response = requests.get(url)
+        # Check if the request was successful
+        if response.status_code == 200:
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+            print("Download complete.")
+        else:
+            print(f"Failed to download. Status code: {response.status_code}")
+    else:
+        print("Dataset already exists.")
+
+if __name__ == "__main__":
+    download_dataset()
